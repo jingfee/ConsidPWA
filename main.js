a36f32ddb2045e1ac9207a58c4c3cd330fea9539
Unknown <jingfee@gmail.com>
2017-11-04 16:40:32 +0100

    SW caching

---
+++ main.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+    console.log('Service Worker is supported');
+  
+    navigator.serviceWorker.register('sw.js')
+        .then(function(swReg) {
+            console.log('Service Worker is registered', swReg);
+        })
+        .catch(function(error) {
+            console.error('Service Worker Error', error);
+        });
+}
