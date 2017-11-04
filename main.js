617463975ac908d6e5679c4850dd41d9c7cfc902
Unknown <jingfee@gmail.com>
2017-11-04 16:48:19 +0100

    SW push base

--- main.js
+++ main.js
@@ -1,11 +1,63 @@
-if ('serviceWorker' in navigator) {
-    console.log('Service Worker is supported');
+const applicationServerPublicKey = '';
+const pushButton = document.querySelector('.js-push-btn');
+
+let isSubscribed = false;
+let swRegistration = null;
+
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+    console.log('Service Worker and Push is supported');
   
     navigator.serviceWorker.register('sw.js')
         .then(function(swReg) {
             console.log('Service Worker is registered', swReg);
+            swRegistration = swReg;
+            initializeUI();
         })
         .catch(function(error) {
             console.error('Service Worker Error', error);
         });
+} 
+else {
+    console.warn('Push messaging is not supported');
+    pushButton.textContent = 'Push Not Supported';
+}
+
+function initializeUI() {
+    pushButton.addEventListener('click', function() {
+        pushButton.disabled = true;
+        if (isSubscribed) {
+            unsubscribeUser();
+        } else {
+            subscribeUser();
+        }
+    });
+
+    // Set the initial subscription value
+    //TODO
+}
+
+function subscribeUser() {
+    //TODO
+}
+
+function unsubscribeUser() {
+    //TODO
+}
+
+function updateBtn() {
+}
+
+function urlB64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+      .replace(/\-/g, '+')
+      .replace(/_/g, '/');
+  
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+  
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
 }
